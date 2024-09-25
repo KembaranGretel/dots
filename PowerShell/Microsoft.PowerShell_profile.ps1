a478a46b1ab9ef1bb5bd7986575f6912142b3232
KembaranGretel <hansel24001@mail.unpad.ac.id>
2024-09-25 08:50:31 +0700

    updates

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -4,10 +4,21 @@ Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineKeyHandler -Key Alt-k -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key Alt-j -Function HistorySearchForward
 
+
+function nvimFolder {Set-Location -Path C:\Users\Hansel\AppData\Local\nvim}
+Set-Alias -Name nconf -Value nvimFolder
+Set-Alias -Name v -Value nvim
+
 function commit ($message) {
-        git add . 
-        git commit -m $message 
-        git push 
+        git add .
+        git commit -m $message
+        git push
+}
+
+function run ($file) {
+        g++ $file
+        ./a.exe
+        rm a.exe
 }
 
 function whereis ($command) {
@@ -15,3 +26,13 @@ function whereis ($command) {
     Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
 }
 
+function prompt {
+  $loc = $executionContext.SessionState.Path.CurrentLocation;
+
+$out = ""
+  if ($loc.Provider.Name -eq "FileSystem") {
+    $out += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+  }
+  $out += "PS $loc$('>' * ($nestedPromptLevel + 1)) ";
+  return $out
+}
